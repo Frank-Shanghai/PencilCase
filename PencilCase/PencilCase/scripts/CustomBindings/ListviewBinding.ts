1a7ba42e099f8b7159d812b83e3eefd3e679d02b
Frank <null.reference@hotmail.com>
2018-05-23 18:04:38 +0800

    #32

--- PencilCase/PencilCase/scripts/CustomBindings/ListviewBinding.ts
+++ PencilCase/PencilCase/scripts/CustomBindings/ListviewBinding.ts
@@ -1,5 +1,14 @@
 ﻿class ListviewBinding implements KnockoutBindingHandler {
     public init(element: any, valueAccessor: () => any, allowBindingAccessor: KnockoutAllBindingsAccessor, viewModel: any, bindingContext: KnockoutBindingContext) {
+        /*
+        Implemented listview custom binding handler.
+        Due to dynamically inserted(KO binding) DOMs are not enhanced by JQM,
+        so need this handler here, once the product collection changed, re-enhance the DOM.
+
+        Note: Note that I did not add any export keyword in this class and the Binding class in Binding.ts.
+        In this way, it makes the members in this file are globally available. Just like the JQuery, KO libraries.
+        */
+
         var value = valueAccessor();
         let subscription: KnockoutSubscription = null;
         if (ko.isObservable(value)) {
--- PencilCase/PencilCase/www/scripts/CustomBindings/ListviewBinding.js
+++ PencilCase/PencilCase/www/scripts/CustomBindings/ListviewBinding.js
@@ -2,6 +2,14 @@ var ListviewBinding = (function () {
     function ListviewBinding() {
     }
     ListviewBinding.prototype.init = function (element, valueAccessor, allowBindingAccessor, viewModel, bindingContext) {
+        /*
+        Implemented listview custom binding handler.
+        Due to dynamically inserted(KO binding) DOMs are not enhanced by JQM,
+        so need this handler here, once the product collection changed, re-enhance the DOM.
+
+        Note: Note that I did not add any export keyword in this class and the Binding class in Binding.ts.
+        In this way, it makes the members in this file are globally available. Just like the JQuery, KO libraries.
+        */
         var value = valueAccessor();
         var subscription = null;
         if (ko.isObservable(value)) {
